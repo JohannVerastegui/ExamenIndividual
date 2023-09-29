2e76ef0053ebfa6c35991146d721b4d15e0ecc25
JohannVerastegui <joanvemu2002@gmail.com>
2023-09-28 21:24:51 -0500

    Producto Tela

---
+++ TiendaArtesania/TiendaArtesania/Tela.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaArtesania
+{
+    class Tela : Producto
+    {
+        public string Tamaño { get; set; }
+        public string Material { get; set; }
+        public string Color { get; set; }
+
+        public Tela(string nombre, decimal precio, string tamaño, string material, string color)
+            : base(nombre, precio)
+        {
+            Tamaño = tamaño;
+            Material = material;
+            Color = color;
+        }
+
+        public override string ObtenerCaracteristicas()
+        {
+            return base.ObtenerCaracteristicas() +
+                $", Tamaño: {Tamaño}, Material: {Material}, Color: {Color}";
+        }
+    }
+}
